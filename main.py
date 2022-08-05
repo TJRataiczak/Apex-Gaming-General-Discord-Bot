0f403ecc839f6b03f3692aef462923f16fd4eafd
TJ Rataiczak <69827401+TJRataiczak@users.noreply.github.com>
2022-08-05 16:30:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,6 @@ BOT_TOKEN = "OTgzNTY2Nzg2MjQwODQzODI3.GTGdx2.N0wvxdueME8fG7uDmrLDktPjSafCGerOh0F
 
 intents = nextcord.Intents.all()
 bot = commands.Bot(intents=intents)
-welcome_messages = ["Welcome to the server!<:apexMagic:886088735505149952>",
-                    "Howdy gamer!<:khuckerEZ:881365671169101854>",
-                    "What's poppin slime?<:apexWICKED:979083594821693531>",
-                    "Wait do I know you?<:brickWait:939759637719306271>",
-                    "Hey got any trades?<:apexReader:906696005741838346>"]
 
 @bot.event
 async def on_ready():
@@ -25,11 +20,10 @@ async def on_member_join(member):
 
     welcome_messages = [f"Welcome to the server!<:apexMagic:886088735505149952>\n{member.mention}",
                         f"Howdy gamer!<:khuckerEZ:881365671169101854>\n{member.mention}",
-                        f"What's poppin slime?<:apexWICKED:979083594821693531>\n{member.mention}",
                         f"Wait do I know you?<:brickWait:939759637719306271>\n{member.mention}",
                         f"Hey got any trades?<:apexReader:906696005741838346>\n{member.mention}"]
 
-    embed = Embed(title= "Testing", description = random.choice(welcome_messages), color= Color.blue())
+    embed = Embed(title= "Welcome!", description = random.choice(welcome_messages), color= Color.blue())
     embed.set_thumbnail(member.avatar)
     embed.set_footer()
     await member.guild.system_channel.send(embed=embed)
@@ -39,7 +33,6 @@ async def socials(interaction: nextcord.Interaction):
 
     embed = Embed(title='Apex Gaming | Linktree', description= "Check out all of our social media links here!",color = Color.blue() , url = "https://linktr.ee/apexgamingesports", timestamp=datetime.datetime.now())
     embed.set_thumbnail(url = "https://d1fdloi71mui9q.cloudfront.net/IwzPyzSnTLS8fdpNcS9W_IIKg6H3OYxV5YYVc")
-    embed.set_footer(text="Developed by TJ Rataiczak#2710", icon_url="https://cdn.discordapp.com/avatars/352190988178948096/fa7da4b911530180131e51e9499e0cec.png?size=1024")
     
     await interaction.send(embed=embed)
     print(interaction.user.avatar)
