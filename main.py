e5226a0d07c34ca9d258f420abef1aa49d186501
TJ Rataiczak <69827401+TJRataiczak@users.noreply.github.com>
2022-11-16 12:56:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -17,23 +17,23 @@ bot = commands.Bot(intents=intents)
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
-@bot.event
-async def on_member_join(member):
+# @bot.event
+# async def on_member_join(member):
 
-    welcome_messages = ["Welcome to the server! <:apexMagic:886088735505149952>",
-    "Howdy gamer! <:khuckerEZ:881365671169101854>",
-    "Wait do I know you? <:brickWait:939759637719306271>",
-    "Hey got any trades? <:apexReader:906696005741838346>"]
+#     welcome_messages = ["Welcome to the server! <:apexMagic:886088735505149952>",
+#     "Howdy gamer! <:khuckerEZ:881365671169101854>",
+#     "Wait do I know you? <:brickWait:939759637719306271>",
+#     "Hey got any trades? <:apexReader:906696005741838346>"]
 
-    embed = Embed(title= f"Welcome {member.display_name} to the server!",description=random.choice(welcome_messages), color= Color.blue())
-    embed.set_thumbnail(member.display_avatar)
-    await member.guild.system_channel.send(embed=embed)
+#     embed = Embed(title= f"Welcome {member.display_name} to the server!",description=random.choice(welcome_messages), color= Color.blue())
+#     embed.set_thumbnail(member.display_avatar)
+#     await member.guild.system_channel.send(embed=embed)
 
-@bot.slash_command(name="socials",description="Gives link to socials", guild_ids=GUILD_IDS)
-async def socials(interaction: nextcord.Interaction):
+# @bot.slash_command(name="socials",description="Gives link to socials", guild_ids=GUILD_IDS)
+# async def socials(interaction: nextcord.Interaction):
 
-    embed = Embed(title='Apex Gaming | Linktree', description= "Check out all of our social media links here!",color = Color.blue(), url = "https://linktr.ee/apexgamingesports")
-    embed.set_thumbnail(url = "https://d1fdloi71mui9q.cloudfront.net/IwzPyzSnTLS8fdpNcS9W_IIKg6H3OYxV5YYVc")    
-    await interaction.send(embed=embed)
+#     embed = Embed(title='Apex Gaming | Linktree', description= "Check out all of our social media links here!",color = Color.blue(), url = "https://linktr.ee/apexgamingesports")
+#     embed.set_thumbnail(url = "https://d1fdloi71mui9q.cloudfront.net/IwzPyzSnTLS8fdpNcS9W_IIKg6H3OYxV5YYVc")    
+#     await interaction.send(embed=embed)
 
 bot.run(BOT_TOKEN)
